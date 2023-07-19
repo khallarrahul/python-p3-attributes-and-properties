0fada9842be0e2708b4f6e64aa98c8423106d79c
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-07-19 13:56:50 -0700

    resolves

--- lib/person.py
+++ lib/person.py
@@ -12,8 +12,37 @@ APPROVED_JOBS = [
     "General Management",
     "Research & Development",
     "Marketing",
-    "Purchasing"
+    "Purchasing",
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="Guido", job="Admin"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if (type(name) == str) and (1 <= len(name) <= 25):
+            self._name = name.title()
+            return self._name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+
+    job = property(get_job, set_job)
+
+
+Person(job="Benevolent dictator for life")
